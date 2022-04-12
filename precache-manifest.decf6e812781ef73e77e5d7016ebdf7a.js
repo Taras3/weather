dddd9ed3ab33ecfefd62c5a809c3d9a640334322
Taras3 <taras-3@i.ua>
2022-04-12 14:04:33 +0300

    Updates

---
+++ precache-manifest.decf6e812781ef73e77e5d7016ebdf7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9260b0010cf19611dcd8",
+    "url": "/weather/static/css/main.57a89171.chunk.css"
+  },
+  {
+    "revision": "9260b0010cf19611dcd8",
+    "url": "/weather/static/js/main.9260b001.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/weather/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "d1cd4a44434480263b9e",
+    "url": "/weather/static/js/2.d1cd4a44.chunk.js"
+  },
+  {
+    "revision": "554f81024dbdc84e20ef",
+    "url": "/weather/static/js/runtime~main.554f8102.js"
+  },
+  {
+    "revision": "59b596d0919033de9cc55a150e93c465",
+    "url": "/weather/index.html"
+  }
+];
